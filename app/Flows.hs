9da5b51e76df1cf342568c59b763c103d87785cf
Martin Ruderer <martin.ruderer@gmail.com>
2022-02-07 23:01:44 +0100

    more flexible flows

--- app/Flows.hs
+++ app/Flows.hs
@@ -19,9 +19,23 @@ import Numeric.LinearAlgebra hiding (scale)
 import qualified Numeric.Noise.Perlin as P
 import Prelude hiding ((<>))
 
+type Flow = Vector Double -> Vector Double
+
 type Color = (Double, Double, Double, Double)
 
 data Particle = P {pCoords :: Vector Double, pColor :: Color, pSize :: Double} deriving (Show)
+
+screen = (-1.5, 1.5)
+
+nrOfParticles = 1000
+
+nrOfIterations = 1000
+
+flow :: Flow
+flow v =
+  let [x, y] = toList v
+      phi = sin (exp (x * y))
+   in (squeeze 1 <> rot phi) #> v
 
 rot :: Double -> Matrix Double
 rot phi = matrix 2 [cos phi, - sin phi, sin phi, cos phi]
@@ -35,13 +49,13 @@ scale k = matrix 2 [k, 0, 0, k]
 squeeze :: Double -> Matrix Double
 squeeze k = matrix 2 [k, 0, 0, 1 / k]
 
-moveParticle :: Matrix Double -> Particle -> Particle
-moveParticle field particle =
-  let nextCoords = pCoords particle + scale 0.001 #> field #> pCoords particle
+moveParticle :: Flow -> Particle -> Particle
+moveParticle flow particle =
+  let nextCoords = pCoords particle + scale 0.001 #> flow (pCoords particle)
       nextParticle = particle {pCoords = nextCoords}
    in nextParticle
 
-moveParticles :: Matrix Double -> [Particle] -> [Particle]
+moveParticles :: Flow -> [Particle] -> [Particle]
 moveParticles field = map (moveParticle field)
 
 teaGreen' :: Color
@@ -59,8 +73,8 @@ darkGunmetal' = (170, 0.30, 0.16, 0.4)
 initParticle :: Generate Particle
 initParticle = do
   (width, height) <- getSize
-  x <- getRandomR (-1, 1)
-  y <- getRandomR (-1, 1)
+  x <- getRandomR screen
+  y <- getRandomR screen
   let coords = vector [x, y]
   color <-
     uniform
@@ -69,23 +83,22 @@ initParticle = do
         englishVermillion',
         darkGunmetal'
       ]
-  return $ P {pCoords = coords, pSize = 1, pColor = color}
+  return $ P {pCoords = coords, pSize = 0.5, pColor = color}
 
 initParticles :: Generate [Particle]
-initParticles = mapM (const initParticle) [0 .. 800]
+initParticles = mapM (const initParticle) [0 .. nrOfParticles]
 
 particles :: Generate [Particle]
 particles = do
   initial <- initParticles
-  let flow = squeeze 2 <> rot 10
-      iterations = iterate (moveParticles flow) initial
-  return $ concat $ take 600 iterations
+  let iterations = iterate (moveParticles flow) initial
+  return $ concat $ take nrOfIterations iterations
 
 drawParticle :: Particle -> Generate ()
 drawParticle particle = do
   (width, height) <- getSize
   let [mx, my] = toList $ pCoords particle
-      (x, y) = (tra width (-1, 1) mx, tra height (-1, 1) my)
+      (x, y) = (tra width screen mx, tra height screen my)
       (r, g, b, a) = pColor particle
   cairo $ setSourceRGBA r g b a
   cairo $ arc x y (pSize particle) 0 (2 * pi)
