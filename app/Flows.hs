bd21a6a9fb03d1b9f9a23f1db5540e0215787722
Martin Ruderer <martin.ruderer@gmail.com>
2022-02-12 22:52:16 +0100

    Flow: Get colors by perlin noise

--- app/Flows.hs
+++ app/Flows.hs
@@ -1,10 +1,14 @@
-{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Flows where
 
 import Control.Arrow ((&&&))
+import Control.DeepSeq
+import Control.DeepSeq.Generics (genericRnf)
 import Control.Monad.Random (MonadRandom (getRandomR), RandT, StdGen, mkStdGen, runRandT, uniform)
 import Control.Monad.Reader
+import Control.Parallel.Strategies (NFData, parMap, rdeepseq, rparWith)
 import Data.Colour
 import Data.Colour.RGBSpace.HSV (hsv)
 import Data.Colour.SRGB
@@ -13,6 +17,7 @@ import Data.Map (Map, findWithDefault)
 import qualified Data.Map as Map
 import Data.VectorSpace (VectorSpace ((*^)), (^+^))
 import Debug.Trace (trace)
+import GHC.Generics
 import Generate
 import Graphics.Rendering.Cairo hiding (Matrix, scale)
 import Numeric.LinearAlgebra hiding (scale)
@@ -23,19 +28,32 @@ type Flow = Vector Double -> Vector Double
 
 type Color = (Double, Double, Double, Double)
 
-data Particle = P {pCoords :: Vector Double, pColor :: Color, pSize :: Double} deriving (Show)
+data Particle = P {pCoords :: Vector Double, pColor :: Color, pSize :: Double} deriving (Show, Generic)
 
-screen = (-1.5, 1.5)
+instance NFData Particle where rnf = genericRnf
 
-nrOfParticles = 1000
+screen = (-1, 1)
 
-nrOfIterations = 1000
+nrOfParticles = 2000
 
+nrOfIterations = 5000
+
+-- possibly don't use these coordinates, but move them to a grid?
 flow :: Flow
 flow v =
   let [x, y] = toList v
-      phi = sin (exp (x * y))
-   in (squeeze 1 <> rot phi) #> v
+      phi = 2 * sin (x * y)
+      flowed = (squeeze 1 <> rot phi) #> v
+      normed = flowed / scalar (norm_2 flowed)
+   in normed
+
+flow2 :: Flow
+flow2 v =
+  let [x, y] = toList v
+      phi = 2 * sin (exp (x * y))
+      flowed = (squeeze 1 <> rot phi) #> v
+      normed = flowed / scalar (norm_2 flowed)
+   in normed
 
 rot :: Double -> Matrix Double
 rot phi = matrix 2 [cos phi, - sin phi, sin phi, cos phi]
@@ -58,17 +76,40 @@ moveParticle flow particle =
 moveParticles :: Flow -> [Particle] -> [Particle]
 moveParticles field = map (moveParticle field)
 
+-- moveParticles field = parMap (rparWith rdeepseq) (moveParticle field)
+
 teaGreen' :: Color
-teaGreen' = (81, 0.25, 0.94, 0.3)
+teaGreen' = (81, 0.25, 0.94, 0.5)
 
 vividTangerine' :: Color
-vividTangerine' = (11, 0.40, 0.92, 0.3)
+vividTangerine' = (11, 0.40, 0.92, 0.5)
 
 englishVermillion' :: Color
-englishVermillion' = (355, 0.68, 0.84, 0.3)
+englishVermillion' = (355, 0.68, 0.84, 0.5)
 
 darkGunmetal' :: Color
-darkGunmetal' = (170, 0.30, 0.16, 0.4)
+darkGunmetal' = (170, 0.30, 0.16, 0.5)
+
+noise :: Vector Double -> Generate Double
+noise v = do
+  perlinSeed <- fromIntegral <$> asks worldSeed
+  let [x, y] = toList v
+      perlinOctaves = 5
+      perlinScale = 0.1
+      perlinPersistance = 0.5
+      perlinNoise =
+        P.perlin (round perlinSeed) perlinOctaves perlinScale perlinPersistance
+      res = P.noiseValue perlinNoise (x + perlinSeed, y + perlinSeed, perlinSeed) - 0.5
+
+  return res
+
+byNoise :: Vector Double -> [a] -> Generate a
+byNoise v options =
+  do
+    n <- noise v
+    let i = round (n * fromIntegral (length options)) `mod` length options
+
+    return $ options !! i
 
 initParticle :: Generate Particle
 initParticle = do
@@ -76,13 +117,13 @@ initParticle = do
   x <- getRandomR screen
   y <- getRandomR screen
   let coords = vector [x, y]
-  color <-
-    uniform
-      [ teaGreen',
-        vividTangerine',
-        englishVermillion',
-        darkGunmetal'
-      ]
+      palette =
+        [ teaGreen',
+          vividTangerine',
+          englishVermillion',
+          darkGunmetal'
+        ]
+  color <- byNoise coords palette
   return $ P {pCoords = coords, pSize = 0.5, pColor = color}
 
 initParticles :: Generate [Particle]
